4dddd6b26ae885c43c83a4478fb79bf52cac171a
Rafael Sampaio Moura <rafaelsm123@outlook.com>
2023-03-30 15:45:31 -0300

    Requisito 14 Completo

--- front-end/src/components/products/ProductCard.jsx
+++ front-end/src/components/products/ProductCard.jsx
@@ -1,13 +1,23 @@
 function ProductCard(props) {
-  const { id } = props;
+  const {
+    id,
+    product: { name, price, urlImage },
+  } = props;
+
+  const properPrice = String(price).replace(".", ",");
+
   return (
     <div>
-      <h2 data-testid={`customer_products__element-card-title-${id}`}>Nome</h2>
-      <p data-testid={`customer_products__element-card-price-${id}`}>Preço</p>
+      <h2 data-testid={`customer_products__element-card-title-${id}`}>
+        {name}
+      </h2>
+      <p data-testid={`customer_products__element-card-price-${id}`}>
+        {properPrice}
+      </p>
       <img
         data-testid={`customer_products__img-card-bg-image-${id}`}
-        src="https://i.imgur.com/0pufkn9.jpeg"
-        alt={`image=${id}`}
+        src={`${urlImage}`}
+        alt={`image=${name}`}
         height={100}
         width={100}
       />
@@ -17,6 +27,7 @@ function ProductCard(props) {
       <input
         data-testid={`customer_products__input-card-quantity-${id}`}
         type="number"
+        defaultValue={0}
       />
       <button data-testid={`customer_products__button-card-rm-item-${id}`}>
         -
--- front-end/src/components/products/ProductDeck.jsx
+++ front-end/src/components/products/ProductDeck.jsx
@@ -1,19 +1,20 @@
+import axios from "axios";
 import React from "react";
 import ProductCard from "./ProductCard";
 
 function ProductDeck() {
   const [protoArr, setProtoArr] = React.useState([]);
 
-  React.useEffect(() => {
-    const arr = Array(11).fill(1);
+  React.useEffect(async () => {
+    const productList = await axios.get("http://localhost:3001/products");
 
-    setProtoArr([...arr]);
+    setProtoArr([...productList.data]);
   }, []);
 
   return (
     <div>
-      {protoArr.map((e, index) => (
-        <ProductCard id={index + 1} key={index + 1} />
+      {protoArr.map((e) => (
+        <ProductCard id={e.id} key={e.id} product={e} />
       ))}
     </div>
   );
