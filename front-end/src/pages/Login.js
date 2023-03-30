279bf87e5e0f87b20f737fdd2debbeb66d013d8c
Emmanuel <emmanuel.redu.rezende@gmail.com>
2023-03-30 15:39:58 -0300

    requisito 4

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -53,7 +53,7 @@ function Login() {
       </button>
       <p
         hidden={ hideDeniedLogin }
-        data-testid="common_register__element-invalid_register"
+        data-testid="common_login__element-invalid-email"
       >
         Login Inválido
       </p>
