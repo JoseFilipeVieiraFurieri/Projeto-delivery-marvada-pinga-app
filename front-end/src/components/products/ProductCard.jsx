a60443018ab560981ab1a12e05d9bd265a3c1c92
Rafael Sampaio Moura <rafaelsm123@outlook.com>
2023-03-30 18:02:05 -0300

    Requisito 16 completo

--- front-end/src/components/products/ProductCard.jsx
+++ front-end/src/components/products/ProductCard.jsx
@@ -1,10 +1,41 @@
+import React from "react";
+
 function ProductCard(props) {
+  const { handlePrice, totalPrice } = props;
+  const [amount, setAmout] = React.useState(0);
+
+  function handleChange(e) {
+    const oldPrice = +amount * price;
+    const newPrice = +e.target.value * price;
+    console.log("Old price: ", oldPrice);
+    console.log("New price: ", newPrice);
+
+    if (e.target.value > amount) {
+      handlePrice(totalPrice + (newPrice - oldPrice));
+    } else {
+      handlePrice(totalPrice - (oldPrice - newPrice));
+    }
+    setAmout(e.target.value);
+  }
   const {
     id,
     product: { name, price, urlImage },
   } = props;
 
   const properPrice = String(price).replace(".", ",");
+
+  function handleAmount(symbol) {
+    return function () {
+      if (symbol === "+") {
+        setAmout(Number(amount) + 1);
+        handlePrice(totalPrice + +price);
+      } else if (symbol === "-" && amount === 0) {
+      } else {
+        setAmout(Number(amount) - 1);
+        handlePrice(totalPrice - price);
+      }
+    };
+  }
 
   return (
     <div>
@@ -21,15 +52,23 @@ function ProductCard(props) {
         height={100}
         width={100}
       />
-      <button data-testid={`customer_products__button-card-add-item-${id}`}>
+      <button
+        data-testid={`customer_products__button-card-add-item-${id}`}
+        onClick={handleAmount("+")}
+      >
         +
       </button>
       <input
         data-testid={`customer_products__input-card-quantity-${id}`}
         type="number"
-        defaultValue={0}
+        min={0}
+        onChange={handleChange}
+        value={amount}
       />
-      <button data-testid={`customer_products__button-card-rm-item-${id}`}>
+      <button
+        data-testid={`customer_products__button-card-rm-item-${id}`}
+        onClick={handleAmount("-")}
+      >
         -
       </button>
     </div>
--- front-end/src/components/products/ProductDeck.jsx
+++ front-end/src/components/products/ProductDeck.jsx
@@ -1,9 +1,16 @@
 import axios from "axios";
 import React from "react";
 import ProductCard from "./ProductCard";
+import { useHistory } from "react-router-dom";
 
 function ProductDeck() {
   const [protoArr, setProtoArr] = React.useState([]);
+  const [totalPrice, setTotalPrice] = React.useState(0);
+  const history = useHistory();
+
+  function handleRedirect() {
+    history.push("/customer/checkout");
+  }
 
   React.useEffect(async () => {
     const productList = await axios.get("http://localhost:3001/products");
@@ -14,8 +21,26 @@ function ProductDeck() {
   return (
     <div>
       {protoArr.map((e) => (
-        <ProductCard id={e.id} key={e.id} product={e} />
+        <ProductCard
+          id={e.id}
+          key={e.id}
+          product={e}
+          handlePrice={setTotalPrice}
+          totalPrice={totalPrice}
+        />
       ))}
+
+      <button
+        data-testid="customer_products__button-cart"
+        disabled={totalPrice === 0}
+        onClick={handleRedirect}
+      >
+        <p data-testid="customer_products__checkout-bottom-value">
+          {String(
+            Math.round((totalPrice + Number.EPSILON) * 100) / 100
+          ).replace(".", ",")}
+        </p>
+      </button>
     </div>
   );
 }
