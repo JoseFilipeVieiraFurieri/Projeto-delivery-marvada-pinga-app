362471883e213e3bbe00a506ebbca639006bd90d
Pedroscsilva <pedroscsilva2001@gmail.com>
2023-04-05 15:33:02 -0300

    feat: Login page concluded

---
+++ front-end/src/tests/mocks/users.mock.js
@@ -0,0 +1,11 @@
+export const adminUser = { data: {
+  id: 1, name: 'Delivery App Admin', email: 'adm@deliveryapp.com', role: 'administrator',
+} };
+
+export const sellerUser = { data: {
+  id: 2, name: 'Fulana Pereira', email: 'fulana@deliveryapp.com', role: 'seller',
+} };
+
+export const customerUser = { data: {
+  id: 3, name: 'Cliente Zé Birita', email: 'zebirita@email.com', role: 'customer',
+} };
